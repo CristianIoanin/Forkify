8a0604f2a7cc56594d83cdb55b139b31075e0d7f
Cristian Ioanin <cristian.ioanin@gmail.com>
2018-04-25 00:40:00 +0300

    Adding CORS proxy to URL request

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
 export const key = '824bb06ce57cd8a2a9ad717f99bcdc00';
 export const url = 'https://food2fork.com/api/';
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { key, url } from '../config';
+import { key, url, proxy } from '../config';
 
 export default class Search {
     constructor(query) {
@@ -8,7 +8,7 @@ export default class Search {
 
     async getResults() {
         try {
-            const res = await axios.get(`${url}search?key=${key}&q=${this.query}`);
+            const res = await axios(`${proxy}${url}search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch(error) {
